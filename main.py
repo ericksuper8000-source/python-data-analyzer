0588ce18bd32700f6bd30314cd7019990c576886
ErickDev <ericksuper80@gmail.com>
2026-02-04 13:55:18 -0600

    Commit 4

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.load_data import load_data
+
+Ruta_Csv = 'data/sales.csv'
+
+Cargar_Csv = load_data(Ruta_Csv)
+
+print(Cargar_Csv.head())
---
+++ src/load_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def load_data (file_path):
+
+    '''
+    Load sales data from a CSV file.
+    :param file_path: Path to the CSV file
+    :return: DataFrame with sales data
+    '''
+
+    return pd.read_csv(file_path)
