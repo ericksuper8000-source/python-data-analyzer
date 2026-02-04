f7c6d1ecd9192549fe48f72a9b61b3fdb22fc278
ErickDev <ericksuper80@gmail.com>
2026-02-04 15:32:24 -0600

    Codigo 02/04/2026

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ Ruta_Csv = 'data/sales.csv'
 Cargar_Csv = load_data(Ruta_Csv)
 
 print(Cargar_Csv.head())
+
+Dates = Cargar_Csv['date']
+
+print (f'--------')
+
+print (f'{Dates}')
+
+for elemento in enumerate(Dates):
+    print (f'{elemento[1]}')
