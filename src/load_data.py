aaa52f1c180be0301e44cd59dc17f05aab4f36db
ErickDev <ericksuper80@gmail.com>
2026-03-08 14:23:52 -0600

    feature1 commit 3

--- src/load_data.py
+++ src/load_data.py
@@ -1,14 +1,16 @@
 import pandas as pd
 
 def load_data(file_path):
-
-    '''
+    """
     Load sales data from a CSV file.
+
     :param file_path: Path to the CSV file
     :return: DataFrame with sales data
-    '''
+    """
 
-    df = pd.read_csv(file_path, sep=None, engine='python')
-    df['date'] = pd.to_datetime(df['date']).dt.date
+    df = pd.read_csv(file_path, sep=None, engine="python")
+
+    # Convert date column to datetime.date
+    df["date"] = pd.to_datetime(df["date"]).dt.date
 
     return df
